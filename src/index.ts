e32b17bce49fa9f2657cd9b83cdd53a105620a61
Ilua Kisialiou <iluxa91@inbox.ru>
2022-12-12 21:18:11 +0300

    endpoint with query added

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,41 @@
-import express, {Request, Response} from 'express'
+import express, {Request, Response} from "express"
 
 
 const app = express()
 const port = process.env.PORT || 3000
 
 const products = [{title: "tomatoes"}, {title: "orange"}]
-const adresses = [{value: "Nezalezhnasti 12"}, {value: "Pobedi 3"}]
+const adresses = [{id: 1, value: "Nezalezhnasti 12"}, {id: 2, value: "Pobedi 3"}]
 
-app.get('/products', (req:Request, res: Response)=>{
-    res.send(products)
+app.get("/products", (req: Request, res: Response) => {
+    if (req.query.title) {
+        const searchParams = req.query.title.toString()
+        res.send(products.filter(p => p.title.indexOf(searchParams) > -1))
+    } else {
+        res.send(products)
+    }
 })
 
-app.get('/adresses',(req:Request, res: Response)=>{
+app.get("/products/:productTitle", (req: Request, res: Response) => {
+    const product = products.find(p => p.title === req.params.productTitle)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
+app.get("/adresses", (req: Request, res: Response) => {
     res.send(adresses)
+})
+
+app.get("/adresses/:id", (req: Request, res: Response) => {
+    const adress = adresses.find(p => p.id === +req.params.id)
+    if (adress) {
+        res.send(adress)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(port, () => {
