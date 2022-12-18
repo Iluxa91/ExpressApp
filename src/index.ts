fb7e11d83eed311f2ed4a9113995fd5eb5ea6354
Ilua Kisialiou <iluxa91@inbox.ru>
2022-12-18 22:08:10 +0300

    delete, post endpoint added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, {Request, Response} from "express"
 
+import bodyParser from "body-parser"
 
 const app = express()
 const port = process.env.PORT || 3000
 
-const products = [{title: "tomatoes"}, {title: "orange"}]
+const products = [{id: 1, title: "tomatoes"}, {id: 2, title: "orange"}]
 const adresses = [{id: 1, value: "Nezalezhnasti 12"}, {id: 2, value: "Pobedi 3"}]
+
+const parserMiddleware = bodyParser({})
+app.use(parserMiddleware)
 
 app.get("/products", (req: Request, res: Response) => {
     if (req.query.title) {
@@ -15,7 +19,32 @@ app.get("/products", (req: Request, res: Response) => {
         res.send(products)
     }
 })
+app.post("/products", (req: Request, res: Response) => {
+    const newProduct = {
+        id: +(new Date()),
+        title: req.body.title
+    }
+    products.push(newProduct)
+    res.status(201).send(newProduct)
 
+})
+app.get("/products/:id", (req: Request, res: Response) => {
+    const product = products.find(p => p.id === +req.params.id)
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+app.delete("/products/:id", (req: Request, res: Response) => {
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].id === +req.params.id) {
+            res.send(204)
+            return
+        }
+    }
+    res.send(404)
+})
 app.get("/products/:productTitle", (req: Request, res: Response) => {
     const product = products.find(p => p.title === req.params.productTitle)
     if (product) {
@@ -24,11 +53,9 @@ app.get("/products/:productTitle", (req: Request, res: Response) => {
         res.send(404)
     }
 })
-
 app.get("/adresses", (req: Request, res: Response) => {
     res.send(adresses)
 })
-
 app.get("/adresses/:id", (req: Request, res: Response) => {
     const adress = adresses.find(p => p.id === +req.params.id)
     if (adress) {
